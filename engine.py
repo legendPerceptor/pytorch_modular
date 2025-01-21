d0dbeec84fca2f10d5246cab4e878b36c65e1272
Yuanjian Liu <yuanjian@uchicago.edu>
2025-01-21 16:05:14 -0600

    fix the SummaryWriter spell error

--- engine.py
+++ engine.py
@@ -2,7 +2,7 @@ import torch
 
 from tqdm.auto import tqdm
 from typing import Dict, List, Tuple
-from torch.utils.tensorboard import SummaryWritter
+from torch.utils.tensorboard import SummaryWriter
 
 def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
