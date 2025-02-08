bcbf5500baf235ca6634342de8fa7b2efa61e4f7
Yuanjian Liu <yuanjian@uchicago.edu>
2025-02-07 23:02:24 -0600

    move package into pytorch_modular folder and adjust install requirements

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "torch",
-        "torchvision",
-        "matplotlib",
-        "requests",
-        "tqdm",
-        "numpy",
+        "torch>=2.0.0, <3.0",
+        "torchvision>=0.15",
+        "matplotlib>=3.10",
+        "requests>=2.32",
+        "tqdm>=4.62",
+        "numpy>=1.26",
     ]
 )
